c5dcb6d87729751a2f3fac3ffbb8a1b3ec454009
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2019-11-05 16:48:21 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com> git-git

--- 165150307111054_dwiki nuridhuha.py
+++ 165150307111054_dwiki nuridhuha.py
@@ -16,23 +16,23 @@ class MyTopo( Topo ):
 	"Create custom topo."
 
         # Initialize topology
-	Topo.__init__( self )
+		Topo.__init__( self )
 
         # Add hosts and switches
-	h1 = self.addHost( 'h1' )
-	h2 = self.addHost( 'h2' )
-	h3 = self.addHost( 'h3' )
-	h4 = self.addHost( 'h4' )
-	h5 = self.addHost( 'h5' )
-	s1 = self.addSwitch( 's1' )
-	s2 = self.addSwitch( 's2' )
+		h1 = self.addHost( 'h1' )
+		h2 = self.addHost( 'h2' )
+		h3 = self.addHost( 'h3' )
+		h4 = self.addHost( 'h4' )
+		h5 = self.addHost( 'h5' )
+		s1 = self.addSwitch( 's1' )
+		s2 = self.addSwitch( 's2' )
 
         # Add links
-	self.addLink(s1, h1)
-	self.addLink(s1, h2)
-	self.addLink(s1, h3)
-	self.addLink(s1, s2)
-	self.addLink(s2, h4)
-	self.addLink(s2, h5)
+		self.addLink(s1, h1)
+		self.addLink(s1, h2)
+		self.addLink(s1, h3)
+		self.addLink(s1, s2)
+		self.addLink(s2, h4)
+		self.addLink(s2, h5)
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
